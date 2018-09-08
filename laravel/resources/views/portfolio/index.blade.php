126d37e148d3d53228edb555b545fb6600f68ea7
YJYJ17 <comyj9078@gmail.com>
2018-09-08 20:01:52 +0900

    add portfolio profile frame

--- laravel/resources/views/portfolio/index.blade.php
+++ laravel/resources/views/portfolio/index.blade.php
@@ -1,10 +1,37 @@
-<!doctype html>
-<html>
-    <div>
-      <div class="user-info">
-        <h2 align="center">김명길</h2>
-        <h4 align="center" style="color:gray;">부산대학교</h4>
-        <h4 align="center" style="color:gray;">정보 보호 및 지능형 IoT 연구실</h4>
-        <h4 align="center" style="font:italic;">clevermk7211@gmail.com</h4>
-      </div>
-</html>
+@extends('layouts.master')
+
+@section('content')
+
+<div class="container">
+	<!--
+  	<div class="row py-3 flex-items-sm-center justify-content-center">
+		<div class="col-md-6 clearfix">
+		  <div class="card profile-card">
+		    <figure>
+		      <img src="http://cps-static.rovicorp.com/3/JPG_400/MI0003/711/MI0003711195.jpg?partner=allrovi.com" class="img-profile">
+		    </figure>
+		    <div class="card-body text-center">
+		      <p class="h4 card-title font-weight-bold">김블록</p>
+		      <p class="h7 card-subtitle text-muted">부산대학교</p>
+		      <p class="h7 card-subtitle text-muted">정보컴퓨터공학</p>
+		      <p class="h7 card-subtitle text-muted">hyper@pusan.ac.kr</p>
+		    </div>
+		  </div>
+		</div>
+	  </div>
+	 -->
+	 
+	 <div class="col-md-8 clearfix mx-auto">
+	 	<div class="card profile-card">
+	 		<img src="/image/profile_happy.jpg" class="img-profile card-img-top">
+	 		<div class="card-body text-center">
+	 			<p class="h4 card-title font-weight-bold">김블록</p>
+	 			<p class="h7 card-subtitle text-muted">부산대학교</p>
+		      	<p class="h7 card-subtitle text-muted">정보컴퓨터공학</p>
+		      	<p class="h7 card-subtitle text-muted">hyper@pusan.ac.kr</p>
+	 		</div>
+	 	</div>
+	 </div>
+</div>
+
+@endsection
