ed4ddfc547425eb2adafd51646255e9e41e38b27
YJYJ17 <comyj9078@gmail.com>
2018-09-08 16:52:49 +0900

    add yj_style.css, portfolio view

---
+++ laravel/resources/views/portfolio/index.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+    <div>
+      <div class="user-info">
+        <h2 align="center">김명길</h2>
+        <h4 align="center" style="color:gray;">부산대학교</h4>
+        <h4 align="center" style="color:gray;">정보 보호 및 지능형 IoT 연구실</h4>
+        <h4 align="center" style="font:italic;">clevermk7211@gmail.com</h4>
+      </div>
+</html>
