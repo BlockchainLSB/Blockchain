1ef21aff03a8fff3a69b31c5271a6c52fae6cecf
bokyoungson <pdn03029@gmail.com>
2018-09-11 19:26:18 +0900

    add nodejs express app

--- jenga/portfolioApp/routes/index.js
+++ jenga/portfolioApp/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/project', function(req, res, next){
+	res.render('project/index', {});
+})
+
 module.exports = router;
