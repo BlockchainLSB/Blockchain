e38e4eee3d8b119a4360e23ef0105f7d62c932a7
YJYJ17 <comyj9078@gmail.com>
2018-09-28 21:50:06 +0900

    final remove token

--- jenga/portfolioApp/routes/index.js
+++ jenga/portfolioApp/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	var sess = req.session;
-	var api_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1MzgxNTgxODUsInVzZXJuYW1lIjoiSmltIiwib3JnTmFtZSI6Ik9yZzEiLCJpYXQiOjE1MzgxMjIxODV9.K72ivhB2Xxau7pa5dRugfLcDz6XtQn-sTi0WDHk_l0U';
+	var api_token = '[put JSON Web Token here]';
 	var api_port = 4000;
 	sess.api_token = api_token;
 	sess.api_port = api_port;
